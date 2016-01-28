cfb6f653e018ac3b1a3215dfc3e8b498d464789a
Sergii Kauk <mail@ck.org.ua>
2016-01-28 15:56:29 +0100

    Remove onLoad function, since wrap does it

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,6 @@
-window.onload=function() {
-
 var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 14,
   center: new google.maps.LatLng(48.2081743, 16.3738189),
 });
 
 new Basemap(map);
-
-}
